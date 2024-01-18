0d8eef79dcba3dd4b587374d1f9051cd0786e151
RAMIT NANDI <136625497+idnantimar@users.noreply.github.com>
2024-01-19 01:34:14 +0530

    Update vanilla_LASSO.py

--- Cat_Select_101/vanilla_LASSO.py
+++ Cat_Select_101/vanilla_LASSO.py
@@ -1,7 +1,7 @@
 """
 Created on Fri Dec 15 11:23:30 2023
 
-Topic: LASSO based Variable Selection, using default configuration of sklearn.
+Topic: LASSO based Variable Selection, using (i)multinomial loss and (ii)One-vs-Rest scheme.
        Since the wide popularity of LASSO , it is used as a baseline for comparison here.
 
 @author: R.Nandi
@@ -11,26 +11,30 @@ Topic: LASSO based Variable Selection, using default configuration of sklearn.
 
 #### Vanilla LASSO ============================================================
 
-from sklearn.linear_model import LogisticRegressionCV
-import warnings
-from sklearn.exceptions import ConvergenceWarning
-from sklearn.base import clone
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegressionCV,LogisticRegression
 from . import My_Template_FeatureImportance
+
 
 
 
 class vanillaLASSO_importance(My_Template_FeatureImportance):
     """
-        Feature selection using Logistic LASSO regression with default configuration of ``LogisticRegressionCV``.
+        Feature selection using Logistic LASSO regression.
+
 
         Class Variables
         ---------------
-        Estimator_Type : ``LogisticRegressionCV(penalty='l1',multi_class='multinomial',solver='saga',scoring=None,cv=None,Cs=10)``
+        Estimator_Type : ``{'penalty':'l1','solver':'saga'}``
 
         Parameters
         ----------
         random_state : int ; default None
             Seed for reproducible results across multiple function calls.
+
+        multiclass : {'multinomial','ovr'} ; default
+            How to handle more than two target classes.
 
         max_features : int ; default None
             The maximum possible number of selection. None implies no constrain,
@@ -45,20 +49,19 @@ class vanillaLASSO_importance(My_Template_FeatureImportance):
         ----------
 
     """
+    Estimator_Type = {'penalty':'l1','solver':'saga'}
 
-    Estimator_Type = LogisticRegressionCV(penalty='l1',multi_class='multinomial',
-                                          solver='saga',
-                                          scoring=None,cv=None,Cs=10)
-    def __init__(self,random_state=None,*,
+    def __init__(self,random_state=None,*,multi_class='multinomial',
                  max_features=None,threshold=1e-10):
         super().__init__(random_state)
+        self.multi_class = multi_class
         self.threshold = threshold
         self.max_features = max_features
 
 
-    def fit(self,X,y,*,n_jobs=None,max_iter=100,reduce_norm=1):
+    def fit(self,X,y,Cs=np.logspace(-4,4,10),*,max_iter=1000,reduce_norm=1,**set_params):
         """
-        ``fit`` method for LASSO.
+        ``fit`` method for ``vanillaLASSO_importance``.
 
         Parameters
         ----------
@@ -69,15 +72,17 @@ class vanillaLASSO_importance(My_Template_FeatureImportance):
         y : Series of shape (n_samples,)
             The target values.
 
-        n_jobs : int ; default None
-            Number of CPU cores used during the cross-validation loop in ``LogisticRegressionCV``.
+        Cs : list of floats ; default ``np.logspace(-2,2,5)``
+            The inverse of regularization strength.
 
-        max_iter : int ; default 100
+        max_iter : int ; default 1000
             Maximum number of iterations of the optimization algorithm.
 
         reduce_norm : non-zero int, inf, -inf ; default 1
             Order of the norm used to compute `feature_importances_` in the case where the `coef_` of the
-            ``LogisticRegressionCV`` is of dimension 2. By default 'l1'-norm is being used.
+            underlying Logistic Regression is of dimension 2. By default 'l1'-norm is being used.
+
+        **set_params : other keyword arguments to ``LogistiRegression()`` or ``LogistiRegressionCV()``
 
         Returns
         -------
@@ -85,19 +90,36 @@ class vanillaLASSO_importance(My_Template_FeatureImportance):
             The fitted ``vanillaLASSO_importance`` instance is returned.
 
         """
-        self.estimator = clone(self.Estimator_Type)
         X = pd.get_dummies(X,drop_first=True,dtype=int)
         super().fit(X,y)
-        self.estimator.set_params(**{'n_jobs':n_jobs,'max_iter':max_iter,'random_state':self.random_state})
-        warnings.filterwarnings("ignore", category=ConvergenceWarning)
-        self.estimator.fit(X,y)
-        warnings.filterwarnings("default", category=ConvergenceWarning)
-        self.coef_ = self.estimator.coef_
-        self.intercept_ = self.estimator.intercept_
-        self.C_ = self.estimator.C_
-        self.Cs_ = self.estimator.Cs_
-        self.feature_importances_ = self._coef_to_importance(self.coef_,reduce_norm,
+        ### fitting the model .....
+        if len(Cs)>1 :
+            estimator = LogisticRegressionCV(**self.Estimator_Type,
+                                             multi_class=self.multi_class,
+                                             Cs=Cs,
+                                             random_state=self.random_state,max_iter=max_iter,
+                                             **set_params)
+            estimator.fit(X,y)
+            self.C_ = estimator.C_
+        else :
+            estimator = LogisticRegression(**self.Estimator_Type,
+                                           multi_class=self.multi_class,
+                                           C=Cs[0],
+                                           random_state=self.random_state,max_iter=max_iter,
+                                           **set_params)
+            estimator.fit(X,y)
+            self.C_ = Cs[0]
+        ### feature_importances .....
+        self.estimator = estimator
+        self.coef_ = estimator.coef_
+        self.intercept_ = estimator.intercept_
+        if self.multi_class=='multinomial' :
+            self.feature_importances_ = self._coef_to_importance(self.coef_,reduce_norm,
                                                              identifiability=False)
+        else :
+            self.feature_importances_ = self._coef_to_importance(self.coef_,reduce_norm,
+                                                             identifiability=True)
+            self.category_specific_importances_ = np.abs(self.coef_)**reduce_norm
         return self
 
 
@@ -170,6 +192,20 @@ class vanillaLASSO_importance(My_Template_FeatureImportance):
         else :
             self.true_support = (self.true_coef >= self.threshold_)
         return super().get_error_rates(plot=plot)
+
+
+    def plot(self,sort=True,savefig=None,**kwargs):
+        ylabel = self.__class__.__name__+'|'+str(self.multi_class)
+        kwargs.update({'ylabel':'vanillaLASSO_'+str(self.multi_class)})
+        super().plot(sort=sort,savefig=savefig,**kwargs)
+
+
+    def dump_to_file(self,file_path=None):
+        if file_path is None :
+            file_path = os.path.join(os.getcwd(),
+                                     f"vanillaLASSO_{self.multi_class}-{datetime.now().strftime('%Y_%m_%d_%H%M%S%f')}.pkl")
+        super().dump_to_file(file_path)
+
 
 
 
