a9848bdd1d33c99d86804f62d360f75d29ced637
RAMIT NANDI <136625497+idnantimar@users.noreply.github.com>
2024-01-18 06:29:53 +0530

    Create __init__.py

---
+++ Cat_Select_101/SVM_importance/__init__.py
@@ -0,0 +1,12 @@
+"""
+Created on Wed Jan 17 15:18:59 2024
+
+Topic: We can use SVM with 'l1' regularization and One-vs-Rest scheme
+       for feature selection.
+
+@author: R.Nandi
+"""
+
+
+
+#### Multiclass L1SVM =========================================================
