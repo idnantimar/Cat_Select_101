3faecff26046ee92e7567df9306d6e2872fe0bad
RAMIT NANDI <136625497+idnantimar@users.noreply.github.com>
2024-01-20 05:19:44 +0530

    Create __init__.py

---
+++ Cat_Select_101/_StabilitySelection/__init__.py
@@ -0,0 +1,8 @@
+"""
+Created on Sun Dec 31 23:10:21 2023
+
+Topic: Stabilty Selection is not any particular method,
+        but a framework that can be applied on any base method for reduced variability on selected support.
+
+@author: R.Nandi
+"""
