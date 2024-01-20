7a00d4a13e3a9d7688f81f285f07cb66b767cb1b
RAMIT NANDI <136625497+idnantimar@users.noreply.github.com>
2024-01-20 15:32:23 +0530

    Update __init__.py

--- Cat_Select_101/Independence_Screening/__init__.py
+++ Cat_Select_101/Independence_Screening/__init__.py
@@ -41,6 +41,73 @@ class SIS_importance(My_Template_FeatureImportance):
 
         Attribures
         ----------
+        classes_ : array of shape (`n_classes_`,)
+            A list of class labels known to the classifier.
+
+        confusion_matrix_for_features_ : array of shape (`n_features_in_`, `n_features_in_`)
+            ``confusion_matrix`` (`true_support`, `support_`)
+
+        false_discoveries_ : array of shape (`n_features_in_`,)
+            Boolean mask of false positives.
+
+        false_negatives_ : array of shape (`n_features_in_`,)
+            Boolean mask of false negatives.
+
+        fdr_ : float
+            1 - ``precision_score`` (`true_support`, `support_`)
+
+        feature_importances_ : array of shape (`n_features_in_`,)
+            Importances of features.
+
+        feature_names_in_ : array of shape (`n_features_in_`,)
+            Names of features seen during ``fit``.
+
+        features_selected_ : array of shape (`n_features_selected_`,)
+            Names of selected features.
+
+        f1_score_for_features_ : float
+            ``f1_score`` (`true_support`, `support_`)
+
+        minimum_model_size_ : int
+            ``np.max`` (`ranking_` [ `true_support` ])
+
+        n_classes_ : int
+            Number of target classes.
+
+        n_false_negatives_ : int
+            Number of false negatives.
+
+        n_features_in_ : array of shape (`n_features_in_`,)
+            Number of features seen during ``fit``.
+
+        n_features_selected_ : int
+            Number of selected features.
+
+        n_samples_ : int
+            Number of observations seen during ``fit``.
+
+        pcer_ : float
+            ``np.mean`` (`false_discoveries_`)
+
+        pfer_ : int
+            ``np.sum`` (`false_discoveries_`)
+
+        ranking_ : array of shape (`n_features_in_`,)
+            The feature ranking, such that ``ranking_[i]`` corresponds to the
+            i-th best feature, i=1,2,..., `n_features_in_`.
+
+        support_ : array of shape (`n_features_in_`,)
+            Boolean mask of selected features.
+
+        threshold_ : float
+            Cut-off in use, for selection/rejection.
+
+        tpr_ : float
+            ``recall_score`` (`true_support`, `support_`)
+
+        true_support : array of shape (`n_features_in_`,)
+            Boolean mask of active features in population, only available after
+            ``get_error_rates`` method is called with true_imp.
 
         References
         ----------
@@ -101,15 +168,21 @@ class SIS_importance(My_Template_FeatureImportance):
         return self
 
 
-    def get_error_rates(self,true_coef,*,plot=False):
+    def get_error_rates(self,true_imp,*,plot=False):
         """
         Computes various error-rates when true importance of the features are known.
 
+        *   If a feature is True in `support_` and False in `true_support`
+            it is a false-discovery or false +ve
+
+        *   If a feature is False in `support_` and True in `true_support`
+            it is a false -ve
+
         Parameters
         ----------
-        true_coef : array of shape (`n_features_in_`,)
+        true_imp : array of shape (`n_features_in_`,)
             If a boolean array , True implies the feature is important in true model, null feature otherwise.
-            If a array of floats , it represent the `feature_importances_` of the true model.
+            If an array of floats , it represent the `feature_importances_` of the true model.
 
         plot : bool ; default False
             Whether to plot the `confusion_matrix_for_features_`.
@@ -118,19 +191,23 @@ class SIS_importance(My_Template_FeatureImportance):
         -------
         dict
             Returns the empirical estimate of various error-rates
-           {'PCER': per-comparison error rate,
-            'FDR': false discovery rate,
-            'PFER': per-family error rate,
-            'TPR': true positive rate
+           {
+               'PCER': per-comparison error rate,
+
+               'FDR': false discovery rate,
+
+               'PFER': per-family error rate,
+
+               'TPR': true positive rate
             }
 
         """
         self.get_support()
-        self.true_coef = np.array(true_coef)
-        if (self.true_coef.dtype==bool) :
-            self.true_support = self.true_coef
+        true_imp = np.array(true_imp)
+        if (true_imp.dtype==bool) :
+            self.true_support = true_imp
         else :
-            self.true_support = (self.true_coef >= self.threshold_)
+            self.true_support = (true_imp >= self.threshold_)
         return super().get_error_rates(plot=plot)
 
 
@@ -168,6 +245,73 @@ class SIScat_importance(My_Template_FeatureImportance):
 
         Attribures
         ----------
+        classes_ : array of shape (`n_classes_`,)
+            A list of class labels known to the classifier.
+
+        confusion_matrix_for_features_ : array of shape (`n_features_in_`, `n_features_in_`)
+            ``confusion_matrix`` (`true_support`, `support_`)
+
+        false_discoveries_ : array of shape (`n_features_in_`,)
+            Boolean mask of false positives.
+
+        false_negatives_ : array of shape (`n_features_in_`,)
+            Boolean mask of false negatives.
+
+        fdr_ : float
+            1 - ``precision_score`` (`true_support`, `support_`)
+
+        feature_importances_ : array of shape (`n_features_in_`,)
+            Importances of features.
+
+        feature_names_in_ : array of shape (`n_features_in_`,)
+            Names of features seen during ``fit``.
+
+        features_selected_ : array of shape (`n_features_selected_`,)
+            Names of selected features.
+
+        f1_score_for_features_ : float
+            ``f1_score`` (`true_support`, `support_`)
+
+        minimum_model_size_ : int
+            ``np.max`` (`ranking_` [ `true_support` ])
+
+        n_classes_ : int
+            Number of target classes.
+
+        n_false_negatives_ : int
+            Number of false negatives.
+
+        n_features_in_ : array of shape (`n_features_in_`,)
+            Number of features seen during ``fit``.
+
+        n_features_selected_ : int
+            Number of selected features.
+
+        n_samples_ : int
+            Number of observations seen during ``fit``.
+
+        pcer_ : float
+            ``np.mean`` (`false_discoveries_`)
+
+        pfer_ : int
+            ``np.sum`` (`false_discoveries_`)
+
+        ranking_ : array of shape (`n_features_in_`,)
+            The feature ranking, such that ``ranking_[i]`` corresponds to the
+            i-th best feature, i=1,2,..., `n_features_in_`.
+
+        support_ : array of shape (`n_features_in_`,)
+            Boolean mask of selected features.
+
+        threshold_ : float
+            Cut-off in use, for selection/rejection.
+
+        tpr_ : float
+            ``recall_score`` (`true_support`, `support_`)
+
+        true_support : array of shape (`n_features_in_`,)
+            Boolean mask of active features in population, only available after
+            ``get_error_rates`` method is called with true_imp.
 
         References
         ----------
@@ -232,15 +376,21 @@ class SIScat_importance(My_Template_FeatureImportance):
         return self
 
 
-    def get_error_rates(self,true_coef,*,plot=False):
+    def get_error_rates(self,true_imp,*,plot=False):
         """
         Computes various error-rates when true importance of the features are known.
 
+        *   If a feature is True in `support_` and False in `true_support`
+            it is a false-discovery or false +ve
+
+        *   If a feature is False in `support_` and True in `true_support`
+            it is a false -ve
+
         Parameters
         ----------
-        true_coef : array of shape (`n_features_in_`,)
+        true_imp : array of shape (`n_features_in_`,)
             If a boolean array , True implies the feature is important in true model, null feature otherwise.
-            If a array of floats , it represent the `feature_importances_` of the true model.
+            If an array of floats , it represent the `feature_importances_` of the true model.
 
         plot : bool ; default False
             Whether to plot the `confusion_matrix_for_features_`.
@@ -249,19 +399,23 @@ class SIScat_importance(My_Template_FeatureImportance):
         -------
         dict
             Returns the empirical estimate of various error-rates
-           {'PCER': per-comparison error rate,
-            'FDR': false discovery rate,
-            'PFER': per-family error rate,
-            'TPR': true positive rate
+           {
+               'PCER': per-comparison error rate,
+
+               'FDR': false discovery rate,
+
+               'PFER': per-family error rate,
+
+               'TPR': true positive rate
             }
 
         """
         self.get_support()
-        self.true_coef = np.array(true_coef)
-        if (self.true_coef.dtype==bool) :
-            self.true_support = self.true_coef
+        true_imp = np.array(true_imp)
+        if (true_imp.dtype==bool) :
+            self.true_support = true_imp
         else :
-            self.true_support = (self.true_coef >= self.threshold_)
+            self.true_support = (true_imp >= self.threshold_)
         return super().get_error_rates(plot=plot)
 
 
