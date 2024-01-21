b8d9db838c1bc1da890627734befd2874069aaa0
RAMIT NANDI <136625497+idnantimar@users.noreply.github.com>
2024-01-22 03:37:04 +0530

    Update discriminative_LeastSquares.py

--- Cat_Select_101/penalized_Regression/discriminative_LeastSquares.py
+++ Cat_Select_101/penalized_Regression/discriminative_LeastSquares.py
@@ -102,7 +102,7 @@ class dLS_impotance(My_Template_FeatureImportance):
 
         gridsearch : a fitted ``GridSearchCV(...)`` object, available when ``len(regularization)``>1
 
-        intercept_ : array of shape (n_classes,)
+        intercept_ : array of shape (`n_classes_`,)
             Intercept added to the decision function.
 
         minimum_model_size_ : int
@@ -114,7 +114,7 @@ class dLS_impotance(My_Template_FeatureImportance):
         n_false_negatives_ : int
             Number of false negatives.
 
-        n_features_in_ : array of shape (`n_features_in_`,)
+        n_features_in_ : int
             Number of features seen during ``fit``.
 
         n_features_selected_ : int
@@ -342,4 +342,3 @@ class dLS_impotance(My_Template_FeatureImportance):
 
 
 #### ==========================================================================
-
