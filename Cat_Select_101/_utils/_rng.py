71a91c42be0424e3c0e6bdaa67e4f6d744d44451
RAMIT NANDI <136625497+idnantimar@users.noreply.github.com>
2024-02-09 03:39:33 +0530

    Add files via upload

---
+++ Cat_Select_101/_utils/_rng.py
@@ -0,0 +1,11 @@
+"""
+Created on Fri Feb  9 03:32:17 2024
+
+Topic: Useful for initializing seeds at None.
+
+@author: R.Nandi
+"""
+
+def _seed_sum(a,b):
+    if (a is None) or (b is None) : return None
+    else : return a+b
